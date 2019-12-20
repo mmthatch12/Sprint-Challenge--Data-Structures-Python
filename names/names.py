ad0250b589ed01063077000507237f67d6b657ff
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-20 10:20:40 -0700

    put binary search tree into names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,43 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value >= self.value and self.right is None:
+            self.right = BinarySearchTree(value)
+        elif value >= self.value and self.right is not None:
+            self.right.insert(value)
+        elif value < self.value and self.left is None:
+            self.left = BinarySearchTree(value)
+        elif value < self.value and self.left is not None:
+            self.left.insert(value)
+
+    def contains(self, target):
+        if target is self.value:
+            return True
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        elif target >= self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def for_each(self, cb):
+        # recursively
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
